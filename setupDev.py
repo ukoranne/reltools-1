f1295e7afd109c3cdaa07ef5a84e8f78e0913c93
Hari Gollapalli <hgollapalli@snaproute.com>
2015-11-06 11:48:35 -0800

    Corrys change for gopacket install

--- setupDev.py
+++ setupDev.py
@@ -129,7 +129,7 @@ def getExternalGoDeps() :
                      },
                      { 'repo'        : 'gopacket',
                        'renamesrc'   : 'gopacket',
-                       'renamedst'   : 'github.com/gopacket'
+                       'renamedst'   : 'github.com/google/gopacket'
                      },
                      ]
 
