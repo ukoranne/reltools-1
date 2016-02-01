56d0c6eb6b306dd2724858f49704bded4a3bfd13
Hari Gollapalli <hgollapalli@snaproute.com>
2016-02-01 15:09:57 -0800

    Minor changes for code gen

--- codegentools/gobind/env.sh
+++ codegentools/gobind/env.sh
@@ -3,12 +3,19 @@
 # source this file to get environment setup for the
 # pyang below here
 
-p=`pwd`
 OPENCONFIG=$SR_CODE_BASE/external/src/openconfig/release
-export PATH=$p/bin:$PATH
-export MANPATH=$p/man:$MANPATH
-export PYTHONPATH=$p:$PYTHONPATH
-export YANG_MODPATH=$p/modules:$OPENCONFIG/models:$YANG_MODPATH
-export PYANG_XSLT_DIR=$p/xslt
-export PYANG_RNG_LIBDIR=$p/schema
-export W=$p
+PYANGDIR=$SR_CODE_BASE/external/src/pyang
+PWD=`pwd`
+
+export PATH=$PATH:$PWD/bin:$PYANGDIR/bin
+export MANPATH=$MANPATH:$PWD/man:$PYANGDIR/man
+export PYTHONPATH=$PYTHONPATH:$PWD:$SR_CODE_BASE/external/src/pyang
+export YANG_MODPATH=$PWD/modules:$OPENCONFIG/models:$YANG_MODPATH
+export PYANG_XSLT_DIR=$PWD/xslt
+export PYANG_RNG_LIBDIR=$PWD/schema
+#export W=$PWD
+
+export YANG_MODPATH=$PYANGDIR/modules:$YANG_MODPATH
+#export PYANG_XSLT_DIR=$PYANGDIR/xslt
+#export PYANG_RNG_LIBDIR=$PYANGDIR/schema
+#export W=$p
